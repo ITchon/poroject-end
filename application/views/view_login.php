957ebb4790a5d5971ace403334d09e3e21febc48
ohm1995o <77139131+ohm1995o@users.noreply.github.com>
2021-02-06 15:48:27 +0700

    Update view_login.php

--- application/views/view_login.php
+++ application/views/view_login.php
@@ -1,10 +1,5 @@
-<body id=grad1>
-<style>
-#grad1 {
+<body style="#F8F8FF">
 
-  background-image: linear-gradient(1deg, rgba(2,0,36,1) 0%, rgba(255,255,255,1) 0%, rgba(213, 117, 72, 1) 100%);
-}
-</style>
 
 		
 				<div class="row">
